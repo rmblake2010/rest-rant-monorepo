b29c80797f2c5f5c8e713b874f99c82957087a3f
Ryan Blake <xbungalowx@gmail.com>
2022-05-31 20:52:07 -0500

    finished

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -3,9 +3,24 @@ import { createContext, useState } from "react";
 
 export const CurrentUser = createContext()
 
+__
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
+    useEffect(() => {
+
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5100/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
+
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
@@ -14,4 +29,7 @@ function CurrentUserProvider({ children }){
     )
 }
 
+
 export default CurrentUserProvider
+
+__
